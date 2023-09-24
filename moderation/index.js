696f2583867900c465dcf89be27d2966951e75dd
JohnGirma <yohannesgirma543@gmial.com>
2023-09-24 19:44:16 +0300

    change localhost

--- moderation/index.js
+++ moderation/index.js
@@ -14,7 +14,7 @@ app.post('/events',async (req, res)=>{
     const {type,data}=req.body
     if (type==='CommentCreatedEvent') {
       const status =data.content.includes('no') ?'rejected' :'approved'
-      await axios.post('http://localhost:4005/events',{
+      await axios.post('http://event-bus-srv:4005/events',{
         type:'CommentModerat',
         data:{
             id:data.id,
